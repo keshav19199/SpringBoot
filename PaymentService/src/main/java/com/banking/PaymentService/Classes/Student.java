2fd55f89f491eb28c6bce890bf0c1c833f5804ec
keshav19199 <keshavwadhavane19@gmail.com>
2026-03-16 22:32:47 +0530

    first commit

---
+++ PaymentService/src/main/java/com/banking/PaymentService/Classes/Student.java
@@ -0,0 +1,15 @@
+package com.banking.PaymentService.Classes;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+
+	//public static void main(String[] args) {
+
+	
+		public Student()
+		{
+			System.out.println("Student Object created..");
+		}
+}
