c6068e2e51857007010c7bfed03da587aeb3a28f
keshav19199 <keshavwadhavane19@gmail.com>
2026-03-26 17:46:28 +0530

    JPA

---
+++ connectivity/src/main/java/com/JPA/connectivity/Controller/BookController.java
@@ -0,0 +1,47 @@
+package com.JPA.connectivity.Controller;
+
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.JPA.connectivity.Entity.Book;
+import com.JPA.connectivity.Repository.Book_Repository;
+
+@RestController
+public class BookController {
+
+	@Autowired
+	public Book_Repository repo;
+	
+	@PostMapping("/add-Book")
+	public String add(@RequestBody Book b)
+	{
+		repo.save(b);
+		return b.getName()+" save successful";
+	}
+	
+	
+	@GetMapping("/get-allbook")
+	public List<Book> get()
+	{
+		return repo.findAll();
+	}
+	
+	
+	@DeleteMapping("/delete/{id}")
+	public String delete(@PathVariable(value="id")int id)
+	{
+		repo.deleteById(id);
+		return "deleted!..";
+	}
+	
+	
+}
