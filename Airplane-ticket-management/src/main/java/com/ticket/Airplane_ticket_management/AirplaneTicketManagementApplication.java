be6c7c263bf7a7989ba1fbe181d45d0abcedf29e
keshav19199 <keshavwadhavane19@gmail.com>
2026-04-07 17:17:27 +0530

    bidirectional project

--- Airplane-ticket-management/src/main/java/com/ticket/Airplane_ticket_management/AirplaneTicketManagementApplication.java
+++ Airplane-ticket-management/src/main/java/com/ticket/Airplane_ticket_management/AirplaneTicketManagementApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AirplaneTicketManagementApplication {
+public class AirplaneTicketManagementApplication {	
 
 	public static void main(String[] args) {
 		SpringApplication.run(AirplaneTicketManagementApplication.class, args);
