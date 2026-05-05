474beaf048e15b7b1037deb51badae21704311be
keshav19199 <keshavwadhavane19@gmail.com>
2026-05-05 15:38:29 +0530

    metro ticket.

---
+++ Ticket-Booking/src/test/java/com/metro/Ticket_Booking/TicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metro.Ticket_Booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
