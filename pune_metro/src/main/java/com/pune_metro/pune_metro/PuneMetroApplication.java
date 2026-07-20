91bfbeefb5c628684a9d95a67b3c9341a9ca8724
keshav19199 <keshavwadhavane19@gmail.com>
2026-07-20 19:11:20 +0530

    pune metro project

---
+++ pune_metro/src/main/java/com/pune_metro/pune_metro/PuneMetroApplication.java
@@ -0,0 +1,13 @@
+package com.pune_metro.pune_metro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuneMetroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuneMetroApplication.class, args);
+	}
+
+}
---
+++ pune_metro/src/test/java/com/pune_metro/pune_metro/PuneMetroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pune_metro.pune_metro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuneMetroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
